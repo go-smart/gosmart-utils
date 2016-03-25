7ff9f78408d7ea43b68547e53785af8a02b9fac5
Phil Weir <phil.weir@numa.ie>
2016-03-25 12:32:02 +0000

    support rfa (i.e. no explicit needle) as well

--- goosefoot/make_needles.sh
+++ goosefoot/make_needles.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 CENTRE=$(grep '<centre ' input.final/settings.xml | sed -e 's/centre/target/g')
+AXIS=$(grep '<needleaxis ' input.final/settings.xml | sed -e 's/.*"\([-0-9.]\+\)".*"\([-0-9.]\+\)".*"\([-0-9.]\+\)".*/\1 \2 \3/g')
 NEEDLES=$(grep '<needle ' input.final/settings.xml | grep offset | sed 's/\(.*\)name\(="[0-9a-zA-Z]"\)\(.*\)/\1name\2 file\2\3/g')
+
+if [ -z "${NEEDLES}" ]; then
+    NEEDLES="<needle offset='0 0 0' axis='$AXIS' name='1' file='1' id='rfa-cylinder-1'/>"
+fi
 
 mkdir -p input.final/needlelibrary
 echo '<needlelibrary scaling="0.001" version="1.0.2">' $CENTRE $NEEDLES '</needlelibrary>' > input.final/needlelibrary/needlelibrary.xml
